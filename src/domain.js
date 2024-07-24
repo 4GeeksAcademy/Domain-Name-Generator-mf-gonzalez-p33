d8153d23174e6b143f4386066a1f8047cc132efe
María Fernanda González <mafergonzalezw@gmail.com>
2024-07-24 02:39:09 +0000

    commit

--- src/domain.js
+++ src/domain.js
@@ -1,11 +1,17 @@
 let pronoun = ["the", "our"];
+
 let adj = ["great", "big"];
+
 let noun = ["jogger", "racoon"];
+
+let domain = ['.com', '.net', '.es', '.org'];
 
 pronoun.forEach((articulo) => {
   adj.forEach((adj) => {
     noun.forEach((noun) => {
-      console.log(articulo + adj + noun + ".com");
+        domain.forEach((domain) => {
+          console.log(articulo + adj + noun + domain);
+      });
     });
   });
 });
